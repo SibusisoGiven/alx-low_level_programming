de145bd81f85e95632b4fd02ed1f9ca4ac8b206f
SibusisoGiven <Sbungwenya360@gmail.com>
2023-07-26 02:58:30 -0700

    _putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 /**
- * _puchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: on success 1.
